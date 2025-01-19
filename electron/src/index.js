fd775c4115a6eb2b444184839d704a7245ba9e5f
hm-yang1 <153845481+hm-yang1@users.noreply.github.com>
2025-01-19 11:18:13 +0800

    Attempt to complie to executable
    - It failed

---
+++ electron/src/index.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron/main')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'build/index.html'),
+    protocol: "file",
+    slashes: "true"
+  }))
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
